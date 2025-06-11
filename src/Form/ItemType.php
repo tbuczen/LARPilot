ea179dc67995e6d2024c7fb7077513d5a4af4f44
Tomasz Buczeń <sirurias@gmail.com>
2025-06-11 16:08:09 +0200

    Add CRUD controllers and forms for places and items

---
+++ src/Form/ItemType.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Larp;
+use App\Entity\StoryObject\Item;
+use Money\Currency;
+use Money\Money;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Yceruto\MoneyBundle\Form\Type\MoneyType;
+
+class ItemType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'form.item.name',
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'form.item.description',
+                'required' => false,
+            ])
+            ->add('isCrafted', CheckboxType::class, [
+                'label' => 'form.item.is_crafted',
+                'required' => false,
+            ])
+            ->add('isPurchased', CheckboxType::class, [
+                'label' => 'form.item.is_purchased',
+                'required' => false,
+            ])
+            ->add('quantity', IntegerType::class, [
+                'label' => 'form.item.quantity',
+            ])
+            ->add('cost', MoneyType::class, [
+                'label' => 'form.item.cost',
+                'currency' => 'USD',
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'form.submit',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Item::class,
+            'translation_domain' => 'forms',
+            'larp' => null,
+        ]);
+
+        $resolver->setRequired('larp');
+        $resolver->setAllowedTypes('larp', Larp::class);
+    }
+}
